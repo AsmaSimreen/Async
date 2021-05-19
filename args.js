a2d04b521366bb000954a1c33a40004628043025
ip11 <iprash011@gmail.com>
2021-05-19 07:44:49 +0530

    cmd args and process env

---
+++ args.js
@@ -0,0 +1,8 @@
+//Command Line Arguments
+console.log(process.argv);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+//Environment Variables
+console.log(process.env.PORT);
+console.log(process.env.name);
